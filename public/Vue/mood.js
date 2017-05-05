732bcf5bca9febae8daac7abe7baae24f87ff0f2
LiLaoLiu <1030809514@qq.com>
2017-05-05 17:30:37 +0800

    .

---
+++ public/Vue/mood.js
@@ -0,0 +1,14 @@
+/*
+ * 测试require
+ * @Author:马少博 (ma.shaobo@qq.com)
+ * @Date: 2017年5月5日16:48:17
+ * @Last Modified by: 马少博
+ * @Last Modified time:2017年5月5日16:48:19
+ */
+
+
+function test() {
+  alert("好的");
+}
+
+exports.test = test;
